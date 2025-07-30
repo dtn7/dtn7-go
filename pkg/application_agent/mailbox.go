aff17dc5b903c3d4382d9226a49005b923193173
Markus Sommer <msommer@informatik.uni-marburg.de>
2025-07-30 12:48:32 +0200

    Create Mailbox-type to handle message delivery

---
+++ pkg/application_agent/mailbox.go
@@ -0,0 +1,271 @@
+// SPDX-FileCopyrightText: 2025 Markus Sommer
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package application_agent
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/dtn7/dtn7-go/pkg/bpv7"
+	"github.com/dtn7/dtn7-go/pkg/store"
+)
+
+type MailboxError struct {
+	bid bpv7.BundleID
+	err error
+}
+
+func NewMailboxError(bid bpv7.BundleID, err error) *MailboxError {
+	merr := MailboxError{
+		bid: bid,
+		err: err,
+	}
+	return &merr
+}
+
+func (merr *MailboxError) Error() string {
+	return fmt.Sprintf("Error delivering Bundle: %v", merr.bid.String())
+}
+
+func (merr *MailboxError) Unwrap() error {
+	return merr.err
+}
+
+type AlreadyDeliveredError struct {
+	bid bpv7.BundleID
+}
+
+func NewAlreadyDeliveredError(bid bpv7.BundleID) *AlreadyDeliveredError {
+	err := AlreadyDeliveredError{
+		bid: bid,
+	}
+	return &err
+}
+
+func (err *AlreadyDeliveredError) Error() string {
+	return fmt.Sprintf("Bundle %v already in mailbox", err.bid.String())
+}
+
+type NoSuchBundleError struct {
+	bid bpv7.BundleID
+}
+
+func NewNoSuchBundleError(bid bpv7.BundleID) *NoSuchBundleError {
+	err := NoSuchBundleError{
+		bid: bid,
+	}
+	return &err
+}
+
+func (err *NoSuchBundleError) Error() string {
+	return fmt.Sprintf("No Bundle with id %v in mailbox", err.bid.String())
+}
+
+// Mailbox provides message storage/querying/delivery for application agents to use.
+// The Mailbox does not store the actual bundles so we don't have to keep all bundles in memory all the time.
+// The boolean values of the messages-map signify whether the bundle has already been retrieved
+type Mailbox struct {
+	rwMutex sync.RWMutex
+
+	messages map[bpv7.BundleID]bool
+}
+
+func NewMailbox() *Mailbox {
+	mailbox := Mailbox{
+		messages: make(map[bpv7.BundleID]bool),
+	}
+	return &mailbox
+}
+
+// Deliver delivers bundle to mailbox.
+// Bundle has to have been stored in the store before delivery
+// Returns MailboxError in case of an error.
+// MailboxError wraps AlreadyDeliveredError if bundle with same BundleID is already stored.
+func (mailbox *Mailbox) Deliver(bndl *bpv7.Bundle) error {
+	mailbox.rwMutex.Lock()
+	defer mailbox.rwMutex.Unlock()
+
+	bid := bndl.ID()
+
+	if _, ok := mailbox.messages[bid]; ok {
+		return NewMailboxError(bid, NewAlreadyDeliveredError(bid))
+	}
+
+	mailbox.messages[bid] = false
+
+	return nil
+}
+
+// List returns a list of the BundleIDs of all bundles stored in the mailbox.
+func (mailbox *Mailbox) List() []bpv7.BundleID {
+	mailbox.rwMutex.RLock()
+	defer mailbox.rwMutex.RUnlock()
+
+	bundles := make([]bpv7.BundleID, 0, len(mailbox.messages))
+	for bndl := range mailbox.messages {
+		bundles = append(bundles, bndl)
+	}
+
+	return bundles
+}
+
+// ListNew returns a list of the BundleIDs of all bundles stored in the mailbox which have not been retrieved before.
+func (mailbox *Mailbox) ListNew() []bpv7.BundleID {
+	mailbox.rwMutex.RLock()
+	defer mailbox.rwMutex.RUnlock()
+
+	bundles := make([]bpv7.BundleID, 0, len(mailbox.messages))
+	for bid := range mailbox.messages {
+		if !mailbox.messages[bid] {
+			bundles = append(bundles, bid)
+		}
+	}
+
+	return bundles
+}
+
+// Get returns the Bundle for a given BundleID.
+// If remove is set, then the bundle will be deleted from the mailbox
+// Returns MailboxError in case of an error.
+// MailboxError wraps either NewNoSuchBundleError or error coming from the store.
+// If no bundle for the given ID is stored, it returns NewNoSuchBundleError
+func (mailbox *Mailbox) Get(bid bpv7.BundleID, remove bool) (*bpv7.Bundle, error) {
+	if remove {
+		mailbox.rwMutex.Lock()
+		defer mailbox.rwMutex.Unlock()
+	} else {
+		mailbox.rwMutex.RLock()
+		defer mailbox.rwMutex.RUnlock()
+	}
+
+	_, ok := mailbox.messages[bid]
+	if !ok {
+		return nil, NewNoSuchBundleError(bid)
+	}
+
+	bd, err := store.GetStoreSingleton().LoadBundleDescriptor(bid)
+	if err != nil {
+		return nil, NewMailboxError(bid, err)
+	}
+
+	bndl, err := bd.Load()
+	if err != nil {
+		return nil, NewMailboxError(bid, err)
+	}
+
+	if remove {
+		delete(mailbox.messages, bid)
+	} else {
+		mailbox.messages[bid] = true
+	}
+
+	return bndl, nil
+}
+
+// GetAll returns slice of all bundles in the mailbox.
+// If remove is set, then the mailbox will be cleared.
+// Returns MailboxError in case of an error.
+// MailboxError wraps error coming from the store.
+// In case of an error, still returns all bundles that were successfully loaded before the error
+func (mailbox *Mailbox) GetAll(remove bool) ([]*bpv7.Bundle, error) {
+	if remove {
+		mailbox.rwMutex.Lock()
+		defer mailbox.rwMutex.Unlock()
+	} else {
+		mailbox.rwMutex.RLock()
+		defer mailbox.rwMutex.RUnlock()
+	}
+
+	bndls := make([]*bpv7.Bundle, 0, len(mailbox.messages))
+	for bid := range mailbox.messages {
+		bd, err := store.GetStoreSingleton().LoadBundleDescriptor(bid)
+		if err != nil {
+			return nil, NewMailboxError(bid, err)
+		}
+
+		bndl, err := bd.Load()
+		if err != nil {
+			return bndls, NewMailboxError(bid, err)
+		}
+
+		bndls = append(bndls, bndl)
+		mailbox.messages[bid] = true
+	}
+
+	if remove {
+		clear(mailbox.messages)
+	}
+
+	return bndls, nil
+}
+
+// GetNew returns slice of all bundles in the mailbox that have not been retrieved before.
+// If remove is set, then the mailbox will be cleared.
+// Returns MailboxError in case of an error.
+// MailboxError wraps error coming from the store.
+// In case of an error, still returns all bundles that were successfully loaded before the error
+func (mailbox *Mailbox) GetNew(remove bool) ([]*bpv7.Bundle, error) {
+	if remove {
+		mailbox.rwMutex.Lock()
+		defer mailbox.rwMutex.Unlock()
+	} else {
+		mailbox.rwMutex.RLock()
+		defer mailbox.rwMutex.RUnlock()
+	}
+
+	bndls := make([]*bpv7.Bundle, 0, len(mailbox.messages))
+	for bid := range mailbox.messages {
+		if mailbox.messages[bid] {
+			continue
+		}
+
+		bd, err := store.GetStoreSingleton().LoadBundleDescriptor(bid)
+		if err != nil {
+			return nil, NewMailboxError(bid, err)
+		}
+
+		bndl, err := bd.Load()
+		if err != nil {
+			return bndls, NewMailboxError(bid, err)
+		}
+
+		bndls = append(bndls, bndl)
+		mailbox.messages[bid] = true
+	}
+
+	if remove {
+		clear(mailbox.messages)
+	}
+
+	return bndls, nil
+}
+
+func (mailbox *Mailbox) Delete(bid bpv7.BundleID) {
+	mailbox.rwMutex.Lock()
+	defer mailbox.rwMutex.Unlock()
+
+	delete(mailbox.messages, bid)
+}
+
+func (mailbox *Mailbox) Clear() {
+	mailbox.rwMutex.Lock()
+	defer mailbox.rwMutex.Unlock()
+
+	clear(mailbox.messages)
+}
+
+// GC runs garbage collection on mailbox
+// Removes all bundles which can longer be loaded from the store (most likely because they have been deleted)
+func (mailbox *Mailbox) GC() {
+	mailbox.rwMutex.Lock()
+	defer mailbox.rwMutex.Unlock()
+
+	for bid := range mailbox.messages {
+		_, err := store.GetStoreSingleton().LoadBundleDescriptor(bid)
+		if err != nil {
+			delete(mailbox.messages, bid)
+		}
+	}
+}
